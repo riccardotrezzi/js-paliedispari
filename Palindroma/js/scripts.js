1de9c54a51ed0272d393ec33b5cd58b3dbddecdd
riccardotrezzi <riccardo.trezzi00@gmail.com>
2024-07-23 15:16:04 +0200

    Esercizio palindroma sistemato

--- Palindroma/js/scripts.js
+++ Palindroma/js/scripts.js
@@ -1,2 +1,23 @@
 const wordUser = prompt('Inserisci una parola:');
 console.log('wordUser', wordUser, wordUser.length, typeof wordUser);
+
+checkPalindrome(wordUser);
+
+function checkPalindrome(word){
+
+    let reverseWord = '';
+    for (let i = word.length - 1; i >= 0; i-- ) {
+        console.log(word[i]);
+
+        reverseWord = reverseWord + word[i];
+    }
+
+    console.log('reverseWord', reverseWord, reverseWord.length , typeof reverseWord);
+
+    if (word == reverseWord){
+        alert('La parola è palindroma!');
+    }
+    else{
+        alert('La parola non è palindroma!');
+    }
+}
